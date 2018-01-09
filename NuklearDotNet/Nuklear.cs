fac8ea7ce6c06f9aa55e378b273960e59d84e791
Saša Barišić <cartman300@net.hr>
2018-01-09 03:56:13 +0100

    Changed output path

--- NuklearDotNet/Nuklear.cs
+++ NuklearDotNet/Nuklear.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NuklearDotNet
-{
-    public class Class1
-    {
-    }
+namespace NuklearDotNet {
+	public static class Nuklear {
+	}
 }
