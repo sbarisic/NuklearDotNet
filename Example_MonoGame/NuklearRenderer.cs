3580de56b9aea3ee73cb8c3afdf1d31aa2e4eb20
sz.krisz20010615 <sz.krisz20010615@gmail.com>
2020-08-31 07:28:04 +0200

    Implemented IFrameBuffer interface. TODO: Implement The UI redraw when the RenderTarget Size Changes. TODO: Implement Mouse and keyboard events.

--- Example_MonoGame/NuklearRenderer.cs
+++ Example_MonoGame/NuklearRenderer.cs
@@ -3,16 +3,20 @@ using Microsoft.Xna.Framework.Graphics;
 using NuklearDotNet;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Example_MonoGame
 {
-    public class NuklearRenderer : NuklearDeviceTex<Texture2D>
+    public class NuklearRenderer : NuklearDeviceTex<Texture2D>, IFrameBuffered
     {
         GraphicsDevice _graphics;
         BasicEffect _basicEffect;
         VertexBuffer _vertexBuffer;
+        RenderTarget2D _renderTarget2D;
+
+        SpriteBatch _spriteBatch;
 
         NkVertex[] _verts;
         ushort[] _inds;
@@ -20,10 +24,31 @@ namespace Example_MonoGame
         public NuklearRenderer(GraphicsDevice graphics)
         {
             this._graphics = graphics;
-            this._basicEffect = new BasicEffect(graphics);
+            this._basicEffect = new BasicEffect(_graphics)
+            {
+                TextureEnabled = true,
+                VertexColorEnabled = true
+            };
 
-            _basicEffect.TextureEnabled = true;
-            _basicEffect.VertexColorEnabled = true;
+            _renderTarget2D = new RenderTarget2D(_graphics,
+                _graphics.PresentationParameters.BackBufferWidth,
+                _graphics.PresentationParameters.BackBufferHeight,
+                false,
+                _graphics.PresentationParameters.BackBufferFormat,
+                DepthFormat.Depth24);
+
+            _spriteBatch = new SpriteBatch(_graphics);
+
+            // This BlendState is no longer neccesary.
+            //BlendState _blendState = new BlendState();
+            //_blendState.ColorBlendFunction = BlendFunction.Add;
+            //_blendState.ColorSourceBlend = Blend.SourceAlpha;
+            //_blendState.ColorDestinationBlend = Blend.InverseSourceAlpha;
+            //SpriteBatch.Begin(SpriteSortMode.Immediate, 
+            //    blendState: _blendstate, 
+            //    samplerState: SamplerState.PointClamp, 
+            //    depthStencilState: DepthStencilState.None, 
+            //    rasterizerState: RasterizerState.CullNone);
         }
 
         public override Texture2D CreateTexture(int W, int H, IntPtr Data)
@@ -72,6 +97,45 @@ namespace Example_MonoGame
         {
             _verts = VertexBuffer;
             _inds = IndexBuffer;
+        }
+
+        public void BeginBuffering()
+        {
+            _graphics.SetRenderTarget(_renderTarget2D);
+            _graphics.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin(SpriteSortMode.Immediate, blendState: BlendState.NonPremultiplied, samplerState: SamplerState.PointClamp, depthStencilState: DepthStencilState.None, rasterizerState: RasterizerState.CullNone);
+        }
+
+        public void EndBuffering()
+        {
+            _spriteBatch.End();
+            _graphics.SetRenderTarget(null);
+            //_graphics.RasterizerState.ScissorTestEnable = false;
+        }
+
+        void IFrameBuffered.RenderFinal()
+        {
+            _spriteBatch.Begin(blendState: BlendState.Opaque);
+            _spriteBatch.Draw(_renderTarget2D, Vector2.Zero, Color.White);
+            _spriteBatch.End();
+
+            RenderFinalEnded();
+        }
+
+        void RenderFinalEnded()
+        {
+            if (_graphics.PresentationParameters.BackBufferWidth != _renderTarget2D.Width
+                || _graphics.PresentationParameters.BackBufferHeight != _renderTarget2D.Height)
+            {
+                _renderTarget2D = new RenderTarget2D(_graphics,
+                _graphics.PresentationParameters.BackBufferWidth,
+                _graphics.PresentationParameters.BackBufferHeight,
+                false,
+                _graphics.PresentationParameters.BackBufferFormat,
+                DepthFormat.None);
+
+                ForceUpdate();
+            }
         }
     }
 }
